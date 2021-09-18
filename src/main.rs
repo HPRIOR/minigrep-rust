eba1ccdbea55f2852a7a8cb628f7daefd9c1bcd1
harry <harryprior@hotmail.co.uk>
2021-09-18 11:59:47 +0100

    Add iterators

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use minigrep::{run, Config};
 
 fn main() {
     // get iterator of cl args and turn into a collection
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+    let config = Config::new(env::args()).unwrap_or_else(|err: &str| {
         eprintln!("error processing arguments: {}", err);
         process::exit(1);
     });
